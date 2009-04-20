9a3ef7e61f74b8cc64bfccdeec891428abc60e99
khorniszon <khorniszon@503780c6-df8a-42dd-a092-7212ff607717>
2009-04-20 12:46:19 +0000

    Missing reflection example added
    git-svn-id: https://tut-framework.svn.sourceforge.net/svnroot/tut-framework/branches/reflection@143 503780c6-df8a-42dd-a092-7212ff607717

---
+++ examples/reflect/ref.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <reflection/reflection.h>
+
+int main(int argc, char **argv)
+{
+    using namespace reflection;
+
+    try
+    {
+        Reflection ref;
+        ref.allow(".*");
+        ref.load(std::string((argc < 2) ? argv[0] : argv[1]));
+        std::cout << ref.toString() << std::endl;
+    }
+    catch(Failure &ex)
+    {
+        std::cout << ex.message() << std::endl;
+    }
+
+    return 0;
+}
